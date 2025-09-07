ae3e1eb9c2d01a80bb572e2a6571da43193d7ebe
Ahmed Ghanem <ahmedghanem0155943@gmail.com>
2025-09-07 19:05:48 +0300

    Generic Repo and UnitOfWork

---
+++ SwiftBuy.Core.Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using SwiftBuy.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftBuy.Core.Domain.Contracts
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+            where TEntity : BaseEntity<TKey>
+            where TKey : IEquatable<TKey>;
+
+        Task<int> CompleteAsync();
+    }
+}
