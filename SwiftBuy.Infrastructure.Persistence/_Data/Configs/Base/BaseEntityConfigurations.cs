8256b6adb5f1ade32c1aba99d555296a11426575
Ahmed Ghanem <ahmedghanem0155943@gmail.com>
2025-09-07 17:07:53 +0300

    Project Structure , Product Entites, SwiftBuyContext, Configs, Migration

---
+++ SwiftBuy.Infrastructure.Persistence/_Data/Configs/Base/BaseEntityConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SwiftBuy.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftBuy.Infrastructure.Persistence._Data.Configs.Base
+{
+    internal class BaseEntityConfigurations<TEntity, TKey> : IEntityTypeConfiguration<TEntity>
+        where TEntity : BaseEntity<TKey>
+        where TKey : IEquatable<TKey>
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.Property(E => E.Id)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}
---
+++ SwiftBuy.Infrastructure.Persistence/_Data/Configs/Products/ProductConfigurations.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SwiftBuy.Core.Domain.Common;
+using SwiftBuy.Core.Domain.Common.Entities;
+using SwiftBuy.Infrastructure.Persistence._Data.Configs.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwiftBuy.Infrastructure.Persistence._Data.Configs.Products
+{
+    internal class ProductConfigurations : BaseAuditableEntityConfigurations<Product, int>
+    {
+        public override void Configure(EntityTypeBuilder<Product> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(180);
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(9,2)");
+
+            builder.HasOne(p => p.Brand)
+                .WithMany()
+                .HasForeignKey(p => p.BrandId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(p => p.Category)
+                .WithMany()
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
