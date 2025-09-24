399031f38c1bb035c3f9a79576c543e957f48cfb
Ahmed Ghanem <ahmedghanem0155943@gmail.com>
2025-09-24 12:02:27 +0300

    Manage Roles, Users in AdminDashboard with MVC Project

---
+++ SwiftBuy.AdminDashboard/Models/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SwiftBuy.AdminDashboard.Models
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<RoleViewModel> Roles { get; set; }
+    }
+}
